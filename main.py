fff911919abdc7cc51f5ff97c6c92cd7aea4c939
Chen Harel <59762611+rufiogd@users.noreply.github.com>
2022-02-02 18:40:30 +0200

    Added option to delete and create files in a loop

--- main.py
+++ main.py
@@ -1,35 +1,83 @@
+from importlib.resources import path
+import os
+
 students = []
+
 def student_list():
     cont = "yes"
     while(cont =="yes"):
-        method=input("What do you want to do? Type 'Load' to open and edit the list, 'View' to view the list, or 'Exit' to exit.").lower()
+        method=input("What do you want to do? Type 'Create' to create a new file, 'Load' to open and edit the file, 'Remove' to delete a file, 'View' to view the file contents, or 'Exit' to exit.").lower()
+        
         if(method == "load"):
-            f = open("student_file.txt", "a")
-            
-            student_dictionary={"name":"", "age":""}
-            name = input("Enter a name:\n").capitalize()
-            age = input("Enter age:\n")
-            
-            student_dictionary.update({"name":name})
-            student_dictionary.update({"age":age})
-            students.append(student_dictionary)
-            
-            save = input("Save and exit? Y or N.").capitalize()
-            
-            if save =="Y":
-                f.write(str(students))
-                f.close()
-                print("List saved to file.")
-                cont = "No"
+            loadFile()
         elif method == "view":
             viewFile()
+        elif method == "create":
+            create()
+        elif method == "remove":
+            remFile()
+        elif method == "exit":
+            cont = "No"
         else:
-            cont = "no"
+            print("Please select a correct option...")
 
 def viewFile():
-    f = open("student_file.txt", "r")
+    fileName = input("What file do you want to view?")
+    f = open(fileName+".txt", "r")
     students = f.read()
     print(students)
+    print("File is loaded...")
     f.close()
+
+def create():
+    fileName = input("Pick a name for your file...\n")
+    f = open(fileName+".txt", "a")
+    print("File has been created...")
+    f.close()
+    newFile = input("Create another file? Y or N.\n").lower()
+    if newFile == "y":
+        create()
+
+def loadFile():
+    cont = "yes"
+    while (cont == "yes"):
+        fileName = input("What file do you want do open?\n")
+        f = open(fileName+".txt", "a")
+
+        student_dictionary = {"name": "", "age": ""}
+        name = input("Enter a name:\n").capitalize()
+        age = input("Enter age:\n")
+
+        student_dictionary.update({"name": name})
+        student_dictionary.update({"age": age})
+        students.append(student_dictionary)
+
+        save = input("Save and exit? Y or N.").capitalize()
+
+        if save == "Y":
+            f.write(str(students))
+            f.close()
+            print("List saved to file.")
+            cont = "No"
+        elif save == "N":
+            cont = "No"
+
+def remFile():
+    r = "yes"
+    while r == "yes":
+        remove = input("Select a file to delete or type 'menu' to go back to the main menu.")
+        if os.path.exists(remove+".txt"):
+            os.remove(remove+".txt")
+            removeAgain = input("Delete another file? Y or N.\n").lower()
+            if removeAgain == "y":
+                remFile()
+            elif removeAgain == "n":
+                student_list()
+            else:
+                print("Select either Y or N.")
+        elif remove == "menu":
+            r = "no"
+        else:
+            print("File does not exist...")
 
 student_list()
